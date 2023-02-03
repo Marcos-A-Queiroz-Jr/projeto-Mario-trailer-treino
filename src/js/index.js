a4e91f77d4b89d9be33f6f4312f2008b9cfa66b3
macos A. Queiroz <124286099+Marcos-A-Queiroz-Jr@users.noreply.github.com>
2023-02-03 20:43:52 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+console.log(document);
+
+console.log (document.querySelector(".botao-trailer"));
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const modal = document.querySelector(".modal");
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+
+console.log(botaoTrailer);
+botaoTrailer.addEventListener("click", () =>{
+    modal.classList.add("aberto")
+    video.setAttribute("src", linkDoVideo);
+});
+
+
+botaoFecharModal.addEventListener("click", () => {
+modal.classList.remove("aberto");
+video.setAttribute("src", "")
+})
+
